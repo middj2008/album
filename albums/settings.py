015931a5945f8d613da35b1f959a31a31adeb0af
middj2008 <35175739+middj2008@users.noreply.github.com>
2021-09-28 20:52:47 +0100

    Modified Model

--- albums/settings.py
+++ albums/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+)4*2+^%vd7***3ek5j&_5zf3fr2!1z2y&nu@6eabc9#n@ba@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
